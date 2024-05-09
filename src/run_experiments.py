796806b9dd65b13cee3ad9542a31328338d7a88c
Alikhan Zimanov <zimanovalikhan07@gmail.com>
2024-05-09 17:11:24 +0000

    exp 2 3 4 code

--- src/run_experiments.py
+++ src/run_experiments.py
@@ -109,6 +109,78 @@ def experiment_2(config):
                 os.remove(temp_config_path)
 
 
+def experiment_3(config):
+    model_tags = [
+        "MyViT",
+    ]
+
+    data_files = [
+        "64k_wpc_10x10_v2.npz",
+        "64k_wpc_25x25.npz",
+        "64k_wpc_40x40.npz",
+    ]
+
+    eng_weights = []
+    for i in range(0, 11):
+        eng_weights.append(i / 10)
+
+    for model_tag in model_tags:
+        for data_file in data_files:
+            for eng_weight in eng_weights:
+                cur_config = copy.deepcopy(config)
+                cur_config['model']['tag'] = model_tag
+                cur_config['paths']['data_file'] = data_file
+                cur_config['training']['eng_loss_weight'] = eng_weight
+
+                temp_config_path = 'temp_config.yaml'
+                with open(temp_config_path, 'w') as temp_config_file:
+                    yaml.safe_dump(cur_config, temp_config_file)
+
+                height, width = extract_dimensions(data_file)
+
+                run_experiment(temp_config_path, f"exp3_{height}x{width}_{eng_weight}")
+
+                os.remove(temp_config_path)
+
+
+def experiment_4(config):
+    model_tags = [
+        "MyViT",
+    ]
+
+    data_files = [
+        "64k_wpc_25x25.npz",
+    ]
+
+    num_layers = [2, 4, 6]
+
+    hidden_dims = [8, 16, 32, 64]
+
+    num_heads = [2, 4, 8]
+
+    for model_tag in model_tags:
+        for data_file in data_files:
+            for num_layer in num_layers:
+                for hidden_dim in hidden_dims:
+                    for n_heads in num_heads:
+                        cur_config = copy.deepcopy(config)
+                        cur_config['model']['tag'] = model_tag
+                        cur_config['paths']['data_file'] = data_file
+                        cur_config['model']['num_layers'] = num_layer
+                        cur_config['model']['hidden_dim'] = hidden_dim
+                        cur_config['model']['num_heads'] = n_heads
+
+                        cur_config['model']['mlp_dim'] = hidden_dim * 4
+
+                        temp_config_path = 'temp_config.yaml'
+                        with open(temp_config_path, 'w') as temp_config_file:
+                            yaml.safe_dump(cur_config, temp_config_file)
+
+                        run_experiment(temp_config_path, f"exp4_{num_layer}_{hidden_dim}_{n_heads}")
+
+                        os.remove(temp_config_path)
+
+
 def main():
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
@@ -127,6 +199,10 @@ def main():
         experiment_1(config)
     elif args.exp_id == 2:
         experiment_2(config)
+    elif args.exp_id == 3:
+        experiment_3(config)
+    elif args.exp_id == 4:
+        experiment_4(config)
     else:
         raise ValueError(f"Invalid experiment id: {args.exp_id}")
 
